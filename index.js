ae3a3f2db03693788c37da8dbe4a8c8041fb94c9
Sandy Mak <sandymak91@gmail.com>
2020-11-22 13:29:11 -0500

    Reformat express app start file to break up db initialization code

--- index.js
+++ index.js
@@ -1,63 +1,72 @@
-const express = require("express");
-const Sequelize = require("sequelize");
+const express = require('express');
+const morgan = require('morgan')
+const db = require('./server/db')
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
-const sequelize = new Sequelize("postgres://localhost:5432/Holiday", {
-  logging: false, // unless you like the logs
-  // ...and there are many other options you may want to play with
-});
+const createApp = () => {
+  // logging middleware
+  app.use(morgan('dev'))
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Connection has been established successfully.");
-  })
-  .catch((err) => {
-    console.error("Unable to connect to the database:", err);
+  // body parsing middleware
+  app.use(express.json())
+  app.use(express.urlencoded({extended: true}))
+
+  app.get("/", (req, res) => res.json({ message: "Hello World" }));
+  app.post("/user", async (req, res) => {
+    try {
+      const newUser = new User(req.body);
+      await newUser.save();
+      res.json({ user: newUser }); // Returns the new user that is created in the database
+    } catch (error) {
+      console.error(error);
+    }
   });
-
-const User = sequelize.define(
-  "user",
-  {
-    // attributes
-    firstName: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    lastName: {
-      type: Sequelize.STRING,
-      // allowNull defaults to true
-    },
-  },
-  {
-    // options
-  }
-);
-// Note: using `force: true` will drop the table if it already exists
-User.sync({ force: true }); // Now the `users` table in the database corresponds to the model definition
-app.get("/", (req, res) => res.json({ message: "Hello World" }));
-app.post("/user", async (req, res) => {
-  try {
-    const newUser = new User(req.body);
-    await newUser.save();
-    res.json({ user: newUser }); // Returns the new user that is created in the database
-  } catch (error) {
-    console.error(error);
-  }
-});
-app.get("/user/:userId", async (req, res) => {
-  const userId = req.params.userId;
-  try {
-    const user = await User.findAll({
-      where: {
-        id: userId,
-      },
-    });
-    res.json({ user });
-  } catch (error) {
-    console.error(error);
+  app.get("/user/:userId", async (req, res) => {
+    const userId = req.params.userId;
+    try {
+      const user = await User.findAll({
+        where: {
+          id: userId,
+        },
+      });
+      res.json({ user });
+    } catch (error) {
+      console.error(error);
   }
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+    // error handling endware
+  app.use((err, req, res, next) => {
+    console.error(err)
+    console.error(err.stack)
+    res.status(err.status || 500).send(err.message || 'Internal server error.')
+  })
+};
+
+const startListening = () => {
+  // start listening (and create a 'server' object representing our server)
+  const server = app.listen(PORT, () =>
+    console.log(`Example app listening on port ${PORT}`)
+  )
+}
+
+// Note: using `{ force: true }` will drop the table if it already exists
+const syncDb = () => db.sync()
+
+async function bootApp() {
+  await syncDb();
+  await createApp();
+  await startListening();
+}
+
+
+// This evaluates as true when this file is run directly from the command line,
+// i.e. when we say 'node server/index.js' (or 'nodemon server/index.js', or 'nodemon server', etc)
+// It will evaluate false when this module is required by another module - for example,
+// if we wanted to require our app in a test spec
+if (require.main === module) {
+  bootApp()
+} else {
+  createApp()
+}
