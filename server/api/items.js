c526c7f0be479721935b846a8620e47a0a2d06a9
Darren Hu <darren.hu3@gmail.com>
2020-12-02 15:20:31 -0500

    updated routes

--- server/api/items.js
+++ server/api/items.js
@@ -2,14 +2,14 @@ const router = require('express').Router();
 const { Item } = require('../db/models');
 module.exports = router;
 
-// get all data by userId
+// get all data by wishlistId
 
 router.get('/:id', async (req, res, next) => {
   try {
-    const userId = req.params.id;
+    const wishlistId = req.params.id;
     const userData = await Item.findAll({
       where: {
-        userId: userId,
+        wishlistId: wishlistId,
       },
     });
     res.json(userData);
@@ -18,3 +18,53 @@ router.get('/:id', async (req, res, next) => {
   }
 });
 
+// remove a item
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const removeItem = await Item.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json(removeItem);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// update a item
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const updateFields = {
+      name: req.body.name,
+      description: req.body.description,
+      image: req.body.image,
+    };
+    const ListInfo = await Item.update(updateFields, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json(ListInfo);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// add new item
+
+router.post('/:id', async (req, res, next) => {
+  try {
+    const newItem = await Item.create({
+      name: req.body.name,
+      description: req.body.description,
+      Image: req.body.image,
+      wishlistId: req.params.id,
+    });
+    res.json(newItem);
+  } catch (error) {
+    next(error);
+  }
+});
--- server/db/models/item.js
+++ server/db/models/item.js
@@ -3,13 +3,13 @@ const db = require('../db');
 
 const Item = db.define('items', {
   name: {
-    type: Sequelize.JSON,
+    type: Sequelize.TEXT,
   },
   description: {
     type: Sequelize.TEXT,
   },
   Image: {
-    type: Sequelize.TEXT,
+    type: Sequelize.STRING,
     defaultValue:
       'https://mpng.subpng.com/20180203/poe/kisspng-gift-red-christmas-birthday-clip-art-small-present-cliparts-5a7581cde5e902.6028480315176503819417.jpg',
   },
