cd199f618c496b250153f5a2f6a811b56efa974c
Darren Hu <darren.hu3@gmail.com>
2020-12-15 17:30:05 -0500

    update user model

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -1,82 +1,83 @@
-const crypto = require('crypto')
-const Sequelize = require('sequelize')
-const db = require('../db')
+const crypto = require('crypto');
+const Sequelize = require('sequelize');
+const db = require('../db');
 
 const User = db.define('user', {
   email: {
     type: Sequelize.STRING,
     unique: true,
-    allowNull: false
+    allowNull: false,
   },
   password: {
     type: Sequelize.STRING,
     // Making `.password` act like a func hides it when serializing to JSON.
     // This is a hack to get around Sequelize's lack of a "private" option.
     get() {
-      return () => this.getDataValue('password')
-    }
+      return () => this.getDataValue('password');
+    },
   },
   salt: {
     type: Sequelize.STRING,
     // Making `.salt` act like a function hides it when serializing to JSON.
     // This is a hack to get around Sequelize's lack of a "private" option.
     get() {
-      return () => this.getDataValue('salt')
-    }
+      return () => this.getDataValue('salt');
+    },
   },
   googleId: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
   },
   name: {
-    type: Sequelize.TEXT
+    type: Sequelize.TEXT,
     // allowNull: false
   },
   address: {
     type: Sequelize.TEXT,
-    allowNull: true
+    allowNull: true,
   },
   profileImage: {
     type: Sequelize.TEXT,
-    defaultValue: ''
-  }
-})
+    defaultValue:
+      'https://res.cloudinary.com/dsi0jbonx/image/upload/v1607114436/snowman_mtdwl8.png',
+  },
+});
 
-module.exports = User
+module.exports = User;
 
 /**
  * instanceMethods
  */
-User.prototype.correctPassword = function(candidatePwd) {
-  return User.encryptPassword(candidatePwd, this.salt()) === this.password()
-}
+User.prototype.correctPassword = function (candidatePwd) {
+  return User.encryptPassword(candidatePwd, this.salt()) === this.password();
+};
 
 /**
  * classMethods
  */
-User.generateSalt = function() {
-  return crypto.randomBytes(16).toString('base64')
-}
+User.generateSalt = function () {
+  return crypto.randomBytes(16).toString('base64');
+};
 
-User.encryptPassword = function(plainText, salt) {
+User.encryptPassword = function (plainText, salt) {
   return crypto
     .createHash('RSA-SHA256')
     .update(plainText)
     .update(salt)
-    .digest('hex')
-}
+    .digest('hex');
+};
 
 /**
  * hooks
  */
-const setSaltAndPassword = user => {
+const setSaltAndPassword = (user) => {
   if (user.changed('password')) {
-    user.salt = User.generateSalt()
-    user.password = User.encryptPassword(user.password(), user.salt())
+    user.salt = User.generateSalt();
+    user.password = User.encryptPassword(user.password(), user.salt());
   }
-}
+};
 
-User.beforeCreate(setSaltAndPassword)
-User.beforeUpdate(setSaltAndPassword)
-User.beforeBulkCreate(users => {
-  users.forEach(setSaltAndPassword)
-})
+User.beforeCreate(setSaltAndPassword);
+User.beforeUpdate(setSaltAndPassword);
+User.beforeBulkCreate((users) => {
+  users.forEach(setSaltAndPassword);
+});
